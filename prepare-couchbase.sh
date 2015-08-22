237eba19f80bb101d7c3db197348d138d439acc0
Tom Ellis <tom.ellis@excelian.com>
2015-08-22 01:53:28 +0100

    try and find some logs

--- prepare-couchbase.sh
+++ prepare-couchbase.sh
@@ -9,13 +9,17 @@ if [ ! -d "$HOME/opt/couchbase" ] ; then
   dpkg-deb -x couchbase-server-enterprise_4.0.0-beta-ubuntu14.04_amd64.deb $HOME
 
   cd $HOME/opt/couchbase
+  rm ./var/lib/couchbase/logs/*
 
   ./bin/install/reloc.sh `pwd`
+
+else
+  cd $HOME/opt/couchbase
+  rm ./var/lib/couchbase/logs/*
 fi
 
 ./bin/couchbase-server -- -noinput -detached
 
-
-ls -lrt
+sleep 30
 
 #./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
--- stop-couchbase.sh
+++ stop-couchbase.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+
+cd $HOME/opt/couchbase
+cat ./var/lib/couchbase/logs/*.log
+
 ./bin/couchbase-server -k
