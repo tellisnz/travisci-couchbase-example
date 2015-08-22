030da159480daae2d1f2cc40e795e060aff90f6c
Tom Ellis <tellisnz@gmail.com>
2015-08-22 17:15:42 +0100

    increase wait

--- prepare-couchbase.sh
+++ prepare-couchbase.sh
@@ -5,6 +5,6 @@ dpkg-deb -x couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb $HOME
 cd $HOME/opt/couchbase
 ./bin/install/reloc.sh `pwd`
 ./bin/couchbase-server -- -noinput -detached
-sleep 5
+sleep 10
 ./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
 ./bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=test --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=512  --bucket-replica=1 -u Administrator -p password
