a6b6d2ab7e36f67b3b3cf4adca24592055477c5f
Tom Ellis <tom.ellis@excelian.com>
2015-08-22 11:27:51 +0100

    go back to 3.1

--- prepare-couchbase.sh
+++ prepare-couchbase.sh
@@ -3,25 +3,26 @@
 # If we haven't installed couchbase yet
 if [ ! -d "$HOME/opt/couchbase" ] ; then
 
-  #wget http://packages.couchbase.com/releases/3.1.0/couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
-  wget http://packages.couchbase.com/releases/4.0.0-beta/couchbase-server-enterprise_4.0.0-beta-ubuntu14.04_amd64.deb
+  wget http://packages.couchbase.com/releases/3.1.0/couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
 
-  #dpkg-deb -x couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb $HOME
-  dpkg-deb -x couchbase-server-enterprise_4.0.0-beta-ubuntu14.04_amd64.deb $HOME
+  dpkg-deb -x couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb $HOME
 
   cd $HOME/opt/couchbase
 
   ./bin/install/reloc.sh `pwd`
 
+  ./bin/couchbase-server -- -noinput -detached
+
+  sleep 30
+
+  ./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
+
+  ./bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=test --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=512  --bucket-replica=1 -u Administrator -p password
+
 else
   cd $HOME/opt/couchbase
-  rm ./var/lib/couchbase/logs/*
+
+  ./bin/couchbase-server -- -noinput -detached
+
+  sleep 30
 fi
-
-./bin/couchbase-server -- -noinput -detached
-
-sleep 30
-
-#./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
-
-#./bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=test --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=512  --bucket-replica=1 -u Administrator -p password
--- stop-couchbase.sh
+++ stop-couchbase.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 $HOME/opt/couchbase/bin/couchbase-server -k
-
-tar -czvf $HOME/logs.tar.gz $HOME/opt/couchbase/var/lib/couchbase/logs/*
