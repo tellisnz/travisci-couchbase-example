69a992d760338cedcc6da75aa7cc23d52d12a878
Tom Ellis <tom.ellis@excelian.com>
2015-08-22 09:32:14 +0100

    init cluster

--- prepare-couchbase.sh
+++ prepare-couchbase.sh
@@ -8,9 +8,9 @@ if [ ! -d "$HOME/opt/couchbase" ] ; then
   #wget http://packages.couchbase.com/releases/4.0.0-beta/couchbase-server-enterprise_4.0.0-beta-ubuntu14.04_amd64.deb
 
   dpkg-deb -x couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb $HOME
+  #dpkg-deb -x couchbase-server-enterprise_4.0.0-beta-ubuntu14.04_amd64.deb $HOME
 
   cd $HOME/opt/couchbase
-  rm ./var/lib/couchbase/logs/*
 
   ./bin/install/reloc.sh `pwd`
 
@@ -23,4 +23,4 @@ fi
 
 sleep 30
 
-#./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
+./bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
